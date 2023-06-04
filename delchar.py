cf00b2d93717fc1dc7ea2462619812c0540aad0b
Cesarjabmx <66344451+Cesarjabmx@users.noreply.github.com>
2023-06-03 21:47:59 -0600

    Create delchar.py
    Elimina el primer y ultimo caracter de una cadena

---
+++ delchar.py
@@ -0,0 +1,8 @@
+str = "Engineering"
+
+print("Original string: " + str)
+
+# Removing first and last characters
+res_str = str[1:-1]
+
+print("String after removal of first and last characters: " + res_str)
